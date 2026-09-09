616c19eda0e8f004f739c9ce7ebd244b5bb4618c
tjsokoloff <tjsokoloff@gmail.com>
2026-09-09 17:26:15 -0400

    Fix formatting of fuel radius print statement

--- dakota_optimization/pinncell_test.py
+++ dakota_optimization/pinncell_test.py
@@ -147,7 +147,7 @@ print(f"Fuel radius = {fuel_or.r:.5f} cm")
 # Generate plot
 openmc.plot_geometry()
 
-print(f"Radius = {fuel_radius:.5f} cm")
+print(f"Radius = {fuel_radius} cm")
 print(f"keff = {keff:.6f}")
 openmc.run()
 
